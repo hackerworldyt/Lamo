4dd4706508a3b9cdd14468de22cb4adc4a977106
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-12-15 22:45:38 +0530

    Update users_sql.py

--- SaitamaRobot/modules/sql/users_sql.py
+++ SaitamaRobot/modules/sql/users_sql.py
@@ -11,11 +11,11 @@ from sqlalchemy import (
     UniqueConstraint,
     func,
 )
-
+from sqlalchemy.sql.sqltypes import BigInteger
 
 class Users(BASE):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     username = Column(UnicodeText)
 
     def __init__(self, user_id, username=None):
