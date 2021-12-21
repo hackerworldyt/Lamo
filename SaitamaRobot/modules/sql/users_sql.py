ada9e70d47d7ed0dfe2f502c137e391815925f5d
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-12-21 17:53:47 +0530

    Update users_sql.py

--- SaitamaRobot/modules/sql/users_sql.py
+++ SaitamaRobot/modules/sql/users_sql.py
@@ -41,7 +41,7 @@ class Chats(BASE):
 
 class ChatMembers(BASE):
     __tablename__ = "chat_members"
-    priv_chat_id = Column(Integer, primary_key=True)
+    priv_chat_id = Column(BigInteger, primary_key=True)
     # NOTE: Use dual primary key instead of private primary key?
     chat = Column(
         String(14),
@@ -49,7 +49,7 @@ class ChatMembers(BASE):
         nullable=False,
     )
     user = Column(
-        Integer,
+        BigInteger,
         ForeignKey("users.user_id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
     )
